7bbde35565d6bc5e43d4805a0e5e2ba68e918d5f
TT0133 <tominaga.ku.info@gmail.com>
2024-10-22 20:58:15 +0900

    Add files via upload

---
+++ javascript/top-script.js
@@ -0,0 +1,19 @@
+// ページ読み込み後に画像をフェードインさせる
+window.onload = function() {
+    const headerImage = document.getElementById('headerImage');
+    headerImage.style.opacity = '1';  // 画像をフェードイン
+};
+
+
+
+
+// Q&Aの折りたたみ機能
+document.addEventListener('DOMContentLoaded', function() {
+    const questions = document.querySelectorAll('.question h3');
+    questions.forEach(question => {
+        question.addEventListener('click', function() {
+            const answer = this.nextElementSibling;
+            answer.style.display = (answer.style.display === 'block') ? 'none' : 'block';
+        });
+    });
+});
